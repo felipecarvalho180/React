26a48cdb12b7b55ec40a250574d21e879686251c
felipecarvalho180 <felipecarvalho180@gmail.com>
2019-03-13 15:15:07 -0300

    Menu1

--- burger/my-app/src/components/Layout/Layout.js
+++ burger/my-app/src/components/Layout/Layout.js
@@ -1,18 +1,32 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Auxiliary from '../../hoc/Auxiliary';
 import classes from './Layout.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const layout = (props) => (
-    <Auxiliary>
-        <Toolbar/>
-        <SideDrawer/>
-        <main className={classes.Content}>
-            {props.children}
-        </main>
-    </Auxiliary>
-);
+class Layout extends Component {
+    state = {
+        showsideDrawer: true
+    }
 
-export default layout;
+    sideDrawerClosedHandler = () => {
+        this.setState({showsideDrawer: false});
+    }
+
+    render() {
+        return(
+            <Auxiliary>
+                <Toolbar/>
+                <SideDrawer 
+                    closed={this.sideDrawerClosedHandler}
+                    open={this.state.showsideDrawer}/>
+                <main className={classes.Content}>
+                    {this.props.children}
+                </main>
+            </Auxiliary>
+        );
+    }
+}
+
+export default Layout;
 
