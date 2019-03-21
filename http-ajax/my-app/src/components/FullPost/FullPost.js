5b0382bfbef56b5fbf08e01e33f059ddd1fd9814
felipecarvalho180 <felipecarvalho180@gmail.com>
2019-03-21 02:33:03 -0300

    interceptors

--- http-ajax/my-app/src/components/FullPost/FullPost.js
+++ http-ajax/my-app/src/components/FullPost/FullPost.js
@@ -20,6 +20,14 @@ class FullPost extends Component {
         }
     }
 
+    deletePostHandler = () => {
+        axios.delete('https://jsonplaceholder.typicode.com/posts/' + this.props.id)
+            .then(response => {
+                //console.log(response);
+                this.setState({loadPost: ""})
+            });
+    }
+
     render () {
         let post = <p style={{textAlign: 'center'}}>Please select a Post!</p>;
         if (this.props.id) {
@@ -31,7 +39,10 @@ class FullPost extends Component {
                     <h1>{this.state.loadPost.title}</h1>
                     <p>{this.state.loadPost.body}</p>
                     <div className="Edit">
-                        <button className="Delete">Delete</button>
+                        <button 
+                            className="Delete"
+                            onClick={this.deletePostHandler}
+                            >Delete</button>
                     </div>
                 </div>
     
