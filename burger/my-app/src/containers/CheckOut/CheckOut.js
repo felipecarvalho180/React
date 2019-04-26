d1851409af4b9828771719291b9800d1b0a47bc7
felipecarvalho180 <felipecarvalho180@gmail.com>
2019-04-26 09:47:35 -0300

    routing

--- burger/my-app/src/containers/CheckOut/CheckOut.js
+++ burger/my-app/src/containers/CheckOut/CheckOut.js
@@ -11,9 +11,29 @@ class CheckOut extends Component {
         }
     }
 
+    componentDidMount() {
+        const query = new URLSearchParams(this.props.location.search);
+        const ingredients = {};
+        for(let param of query.entries()){
+            ingredients[param[0]] = +param[1];
+        }
+        this.setState({ingredients: ingredients});
+    }
+
+    checkoutCancelledHandler = () => {
+        this.props.history.goBack();
+    }
+
+    checkoutContinuedHandler = () => {
+        this.props.history.replace('/checkout/contact-data');
+    }
+
     render(){
         return(
-            <CheckOutSummary ingredients={this.state.ingredients}/>
+            <CheckOutSummary 
+                ingredients={this.state.ingredients}
+                checkoutCancelled={this.checkoutCancelledHandler}
+                checkoutContinued={this.checkoutContinuedHandler}/>
         )
     }
     
